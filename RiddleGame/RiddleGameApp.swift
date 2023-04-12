7d937cda25504037779e06ae305140f3fdc0f007
Biagio <biagior00@gmail.com>
2023-04-12 19:10:49 +0200

    Initial Commit

---
+++ RiddleGame/RiddleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiddleGameApp.swift
+//  RiddleGame
+//
+//  Created by Biagio Ricci on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RiddleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
